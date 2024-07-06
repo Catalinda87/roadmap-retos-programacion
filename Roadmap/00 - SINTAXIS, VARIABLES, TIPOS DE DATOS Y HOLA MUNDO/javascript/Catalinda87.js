5292cf2ccdb0b162091268b9acc768946860cfa0
Catalinda87 <140855771+Catalinda87@users.noreply.github.com>
2024-07-05 21:01:52 -0400

    Catalinda87.js

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/Catalinda87.js
@@ -0,0 +1,31 @@
+/* https://developer.mozilla.org/ */
+
+//Comentario en una línea 
+
+/* Comentario
+En varias 
+lineas.
+*/
+
+let variable = "Nombre de Variable";
+
+const Pi= 3,14159;
+
+
+// let es una variables cuyo valor puede cambiar
+let nombre = "Catalina";
+// String es para datos de texto
+let Saludo = "Hola, mundo";
+// number guarda valores numéricos
+let edad = "37";
+// Array listas donde puedes guardar multiples valores
+let colores = ["Blanco", "Azul", "Rojo"];
+//Booleanos guardan valores de verdadero y falso
+let estudiante = true;
+
+//imprimir hola mundo
+
+let nombreCodigo = "JavaScript";
+console.log("Hola, " + nombreCodigo);
+
+
